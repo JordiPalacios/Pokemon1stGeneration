d3b8e5dbfa33aa521e868832469ac8c37bc39269
unknown <jordipalacioseso@gmail.com>
2023-11-12 13:31:01 +0100

    ImagesBugFixedV2

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const PageNotFound = () => {
     return ( 
         <div className="urlNotFound">
-            <img src="src\assets\img\snorlax.png" alt="Snorlax"/>
+            <img src="src/assets/img/snorlax.png" alt="Snorlax"/>
             <h1>Error... Pokemon Not Foud</h1>
             <Link to={`/`}>
                 <button>Go Home</button>
