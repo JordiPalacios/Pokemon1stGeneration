5fa5ae3ba0b6825c939c0485799dc4334d944774
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-05 20:36:37 +0200

    optimizado

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -6,28 +6,17 @@ import { firstUpperCase } from '../FunctionsJS/FunctionsJS'
 import { PokemonFavouriteContext } from '../context/pokemonFavouriteContext'
 
 export const PokemonDetails = () => {
-    const { getPokemonByName } = useContext(PokemonContext)
 
     const [loading, setLoading] = useState(true)
-    // const [pokemon, setPokemon] = useState({})
-
-    // const { pokemonName } = useParams()
     const {pokemonFavourite, toggleFavouritePokemon} = useContext(PokemonFavouriteContext);
     const favouriteState = {
-      favourite: '★',
-      notFavourite: '☆'
+        favourite: '★',
+        notFavourite: '☆'
     }
-  
-    const handleClick = () => {   
-      toggleFavouritePokemon(pokemon.name)
-    } 
 
-    // //Request details about the Pokemon selected
-    // const fetchPokemon = async name => {
-    //     const data = await getPokemonByName(name)
-    //     setPokemon(data)
-    //     setLoading(false)
-    // }
+    const handleClick = () => {   
+        toggleFavouritePokemon(pokemon.name)
+    } 
 
     const { pokemonName } = useParams();
     const navigate = useNavigate();
@@ -61,60 +50,60 @@ export const PokemonDetails = () => {
         return <p>Loading...</p>;
     }
 
-  return (
-    <main>
-        {
-            loading ? (
-                <Loader />
-            ) : (
-                <>
-                    <div className="pokemonDetailsContainer">
-                        <div className="pokemonDetailsCard">
-                            <div className="pokemonDetailsButton">
-                                <Link to={`/pokemon`}>
-                                    <button>
-                                        X
-                                    </button>
-                                </Link>
-                                <div className='favouritePokemon'>
-                                    <button onClick={handleClick}> {pokemonFavourite[pokemon.name] ? favouriteState.favourite : favouriteState.notFavourite} </button>                    
+    return (
+        <main>
+            {
+                loading ? (
+                    <Loader />
+                ) : (
+                    <>
+                        <div className="pokemonDetailsContainer">
+                            <div className="pokemonDetailsCard">
+                                <div className="pokemonDetailsButton">
+                                    <Link to={`/pokemon`}>
+                                        <button>
+                                            X
+                                        </button>
+                                    </Link>
+                                    <div className='favouritePokemon'>
+                                        <button onClick={handleClick}> {pokemonFavourite[pokemon.name] ? favouriteState.favourite : favouriteState.notFavourite} </button>                    
+                                    </div>
+                                </div>
+                                <div>
+                                    <img align="center"
+                                    src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${pokemon.name}.gif`}
+                                    alt={pokemon.name} />
+                                </div>
+                                <div className='pokemonDetailsText'>
+                                    <h3>{firstUpperCase(pokemon.name)}</h3>
+                                    <ul>
+                                        <h4>Id:&nbsp;&nbsp;</h4>
+                                        <p>{pokemon.id}</p>
+                                    </ul>
+                                    <ul>
+                                        <h4>Type:&nbsp;&nbsp;</h4>
+                                        {pokemon.types.map((type, index)=> (
+                                            <p key={index}>{firstUpperCase(type.type.name)}</p>
+                                            ))}
+                                    </ul>
+                                    <ul>
+                                        <h4>Height:&nbsp;&nbsp;</h4>
+                                        <p>{pokemon.height}</p>
+                                    </ul>
+                                    <ul>
+                                        <div>
+                                            <h4>Habilities:</h4>
+                                            {pokemon.abilities.map((ability, index) => (
+                                                <li key={index}>{firstUpperCase(ability.ability.name)}</li>
+                                                ))}
+                                        </div>
+                                    </ul>
                                 </div>
                             </div>
-                            <div>
-                                <img align="center"
-                                src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${pokemon.name}.gif`}
-                                alt={pokemon.name} />
-                            </div>
-                            <div className='pokemonDetailsText'>
-                                <h3>{firstUpperCase(pokemon.name)}</h3>
-                                <ul>
-                                    <h4>Id:&nbsp;&nbsp;</h4>
-                                    <p>{pokemon.id}</p>
-                                </ul>
-                                <ul>
-                                    <h4>Type:&nbsp;&nbsp;</h4>
-                                    {pokemon.types.map((type, index)=> (
-                                        <p key={index}>{firstUpperCase(type.type.name)}</p>
-                                        ))}
-                                </ul>
-                                <ul>
-                                    <h4>Height:&nbsp;&nbsp;</h4>
-                                    <p>{pokemon.height}</p>
-                                </ul>
-                                <ul>
-                                    <div>
-                                        <h4>Habilities:</h4>
-                                        {pokemon.abilities.map((ability, index) => (
-                                            <li key={index}>{firstUpperCase(ability.ability.name)}</li>
-                                            ))}
-                                    </div>
-                                </ul>
-                            </div>
                         </div>
-                    </div>
-                </>
-            )
-        }
-    </main>
-  )
+                    </>
+                )
+            }
+        </main>
+    )
 }
