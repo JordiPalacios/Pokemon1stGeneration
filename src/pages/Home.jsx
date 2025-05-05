6c534d196eba238c0bdf9af2959c852f221598c8
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-05 20:13:11 +0200

    modificaciones realizadas para mejorar los estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,6 +42,7 @@ export const Home = () => {
             onClick={handleClick}>
             <img src="https://raw.githubusercontent.com/JordiPalacios/Fotos/3300f4522e3ae238ba3076d80cb6cecb4bfcd3ac/pokemonLogo.svg" alt="pokemonLogo" />
           </button>
+          <a href="https://jordicode.dev/" target="_blank" rel="noopener noreferrer">jordicode.dev</a>
           <h1>Generation 1</h1>
           <h2>{pokemons.length} Pokémon</h2>
           <form className='form' onSubmit={handleSubmit}>
